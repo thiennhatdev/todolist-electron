2637744c31e60f6ecc7241a2119f96506c1ad709
tuanhien27 <vodanhtieutot27@gmail.com>
2023-03-23 19:35:07 +0700

    aa

--- forge.config.js
+++ forge.config.js
@@ -4,14 +4,7 @@ module.exports = {
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
-<<<<<<< HEAD
       config: {},
-=======
-      config: {
-        certificateFile: './cert.pfx',
-        certificatePassword: process.env.CERTIFICATE_PASSWORD,
-      },
->>>>>>> e0df385f412d5a3ed94e613f032ece40f98cf269
     },
     {
       name: '@electron-forge/maker-zip',
